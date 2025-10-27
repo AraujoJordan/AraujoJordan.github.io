fa61f8cd2a1883950cb024d330b8e59c125aca74
Jordan L. Araujo Jr. <jordanjr.92@gmail.com>
2025-10-26 19:58:14 -0600

    Add loading screen

--- composeApp.js
+++ composeApp.js
@@ -81,7 +81,7 @@ eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpa
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 "use strict";
-module.exports = __webpack_require__.p + "76705d5d7b352825c85f.wasm";
+module.exports = __webpack_require__.p + "ae8e1f9efab86c87db33.wasm";
 
 /***/ }),
 
