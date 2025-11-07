d5e32406313d454b7d7d5c414986c527ce197497
Jordan L. Araujo Jr. <jordanjr.92@gmail.com>
2025-11-06 21:19:27 -0700

    Add Dice app

--- composeApp.js
+++ composeApp.js
@@ -81,7 +81,7 @@ eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpa
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 "use strict";
-module.exports = __webpack_require__.p + "ba5e4b342d568afcdc12.wasm";
+module.exports = __webpack_require__.p + "f9719cbfe15496fe0253.wasm";
 
 /***/ }),
 
